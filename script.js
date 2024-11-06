7ac0177bf25fb1d62277e932f07b54dbaa1fc990
jcarmichael9 <526jcarmichael@frhsd.com>
2024-11-06 10:39:32 -0500

    everything is setup

---
+++ script.js
@@ -0,0 +1,35 @@
+//these are the variable prompts//
+//Name variable//
+let userName = prompt ('What did your parents write as your name on your birth certificate? (In the first and last name boxes)')
+
+//Name variable//
+let userAge = prompt ('How years have passed since your mom birthed you?')
+
+//Name variable//
+let userName = prompt ('What did your parents write as your name on your birth certificate? (In the first and last name boxes)')
+
+//Name variable//
+let userAge = prompt ('How years have passed since your mom birthed you?')
+
+//Name variable//
+let userName = prompt ('What did your parents write as your name on your birth certificate? (In the first and last name boxes)')
+
+//Name variable//
+let userAge = prompt ('How years have passed since your mom birthed you?')
+
+//Name variable//
+let userName = prompt ('What did your parents write as your name on your birth certificate? (In the first and last name boxes)')
+
+//Name variable//
+let userAge = prompt ('How years have passed since your mom birthed you?')
+
+
+//these are the console logs to show the variables//
+console.log(userName)
+console.log(userAge)
+
+//this the story text//
+let myStory = ``
+console.log(myStory)
+//this connects the story to the id called story//
+document.getElementById('story').innerHTML = myStory
