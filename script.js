021992666943f28f0538385be8e278f5b08fac64
jcarmichael9 <526jcarmichael@frhsd.com>
2024-11-06 19:32:33 -0500

    added the actual madlibs and the terms just need to style

--- script.js
+++ script.js
@@ -2,34 +2,56 @@
 //Name variable//
 let userName = prompt ('What did your parents write as your name on your birth certificate? (In the first and last name boxes)')
 
-//Name variable//
-let userAge = prompt ('How years have passed since your mom birthed you?')
+//first adjective variable//
+let firstAdjective = prompt ('Conceive of a suitable descriptive term')
 
 //Name variable//
-let userName = prompt ('What did your parents write as your name on your birth certificate? (In the first and last name boxes)')
+let bodyPart = prompt ('Specify an anatomical feature')
 
 //Name variable//
-let userAge = prompt ('How years have passed since your mom birthed you?')
+let secondAdjective = prompt ('Conceive of a suitable descriptive term')
 
 //Name variable//
-let userName = prompt ('What did your parents write as your name on your birth certificate? (In the first and last name boxes)')
+let thirdAdjective = prompt ('Conceive of a suitable descriptive term')
 
 //Name variable//
-let userAge = prompt ('How years have passed since your mom birthed you?')
+let firstVerb = prompt ('Conjure an appropriate action term')
 
 //Name variable//
-let userName = prompt ('What did your parents write as your name on your birth certificate? (In the first and last name boxes)')
+let animal = prompt ('Identify a creature from the animal kingdom')
 
 //Name variable//
-let userAge = prompt ('How years have passed since your mom birthed you?')
+let vehicle = prompt ('Envision a mode of transport')
+
+//Name variable//
+let secondVerb = prompt ('Conjure an appropriate action term')
+
+//Name variable//
+let schoolClass = prompt ('Conjure an educational subject')
+
+//Name variable//
+let secondBodyPart = prompt ('Specify an anatomical feature')
+
+//Name variable//
+let time = prompt ('Indicate a span of time, i.e. 2 minutes')
+
+//Name variable//
+let place = prompt ('Identify a location that evokes disdain')
+
+//Name variable//
+let negAdjecticve = prompt ('Select an adjective that conveys negativity or unfavorable qualities')
 
 
-//these are the console logs to show the variables//
-console.log(userName)
-console.log(userAge)
 
 //this the story text//
-let myStory = ``
+let myStory = `I don't know how I put up with ${userName}. Every single day, they walk around with their ${firstAdjective} 
+${bodyPart} held high, acting like they're the ${secondAdjective} gift to this planet. Just hearing their ${thirdAdjective} voice makes 
+me want to ${firstVerb}.
+
+Their laugh? It's like listening to a ${animal} getting run over by a ${vehicle}. And don't even get me started on how they ${secondVerb} during ${schoolClass} class. 
+It's so annoying, it makes my ${secondBodyPart} twitch.
+
+Every moment with them feels like ${time} in ${place}. Honestly, if there was an award for being the most ${negAdjecticve} person, ${userName} would win it hands down. `
 console.log(myStory)
 //this connects the story to the id called story//
 document.getElementById('story').innerHTML = myStory
