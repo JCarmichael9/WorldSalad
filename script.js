1673cb3afa67b244f71d3bd7188955b16949b5ce
jcarmichael9 <526jcarmichael@frhsd.com>
2024-11-19 09:09:45 -0500

    fixed the title and made the prompt words bolded

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ let secondAdjective = prompt ('Conceive of a suitable descriptive term')
 let thirdAdjective = prompt ('Conceive of a suitable descriptive term')
 
 //verb variable//
-let firstVerb = prompt ('Conjure an appropriate action term')
+let firstVerb = prompt ('Conjure an appropriate action term. Present tense not ending in -ing i.e. run')
 
 //animal variable//
 let animal = prompt ('Identify a creature from the animal kingdom')
@@ -24,7 +24,7 @@ let animal = prompt ('Identify a creature from the animal kingdom')
 let vehicle = prompt ('Envision a mode of transport')
 
 //verb variable//
-let secondVerb = prompt ('Conjure an appropriate action term')
+let secondVerb = prompt ('Conjure an appropriate action term. Present tense not ending in -ing i.e. run')
 
 //subject variable//
 let schoolClass = prompt ('Conjure an educational subject')
@@ -44,14 +44,15 @@ let negAdjecticve = prompt ('Select an adjective that conveys negativity or unfa
 
 
 //this the story text//
-let myStory = `I don't know how I put up with ${userName}. Every single day, they walk around with their ${firstAdjective} 
-${bodyPart} held high, acting like they're the ${secondAdjective} gift to this planet. Just hearing their ${thirdAdjective} voice makes 
-me want to ${firstVerb}.
+let myStory = `I don't know how I put up with <b>${userName}</b>. Every single day, they walk around with their <b>${firstAdjective}</b> 
+<b>${bodyPart}</b> held high, acting like they're the <b>${secondAdjective}</b> gift to this planet. Just hearing their <b>${thirdAdjective}</b> voice makes 
+me want to <b>${firstVerb}</b>.
 
-Their laugh? It's like listening to a ${animal} getting run over by a ${vehicle}. And don't even get me started on how they ${secondVerb} during ${schoolClass} class. 
-It's so annoying, it makes my ${secondBodyPart} twitch.
+Their laugh? It's like listening to a <b>${animal}</b> getting run over by a <b>${vehicle}</b>. And don't even get me started on how they <b>${secondVerb}</b> during 
+<b>${schoolClass}</b> class. It's so annoying, it makes my <b>${secondBodyPart}</b> twitch.
 
-Every moment with them feels like ${time} in ${place}. Honestly, if there was an award for being the most ${negAdjecticve} person, ${userName} would win it hands down. `
+Every moment with them feels like <b>${time}</b> in <b>${place}</b>. Honestly, if there was an award for being the most <b>${negAdjecticve}</b> person, <b>${userName}</b>
+ would win it hands down.`
 console.log(myStory)
 //this connects the story to the id called story//
 document.getElementById('story').innerHTML = myStory
